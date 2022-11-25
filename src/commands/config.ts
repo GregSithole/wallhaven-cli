0f32ce78dfcc04888e28b2b9574a58e64b082150
Greg Sithole <gregsithole@hotmail.co.za>
2022-11-25 08:35:56 +0200

    Updated config to use file tree selection

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -1,6 +1,10 @@
 import ora from 'ora';
 import inquirer from 'inquirer';
+import inquirerFileTreeSelection from 'inquirer-file-tree-selection-prompt';
 import conf from 'conf';
+import { getUserHome } from './utils.js';
+
+inquirer.registerPrompt('file-tree-selection', inquirerFileTreeSelection);
 
 const configuration = new conf();
 
@@ -22,9 +26,11 @@ async function checkUserConfigurationValues(options: any) {
 
 	if (!options.downloadDirectory) {
 		questions.push({
-			type: 'input',
+			type: 'file-tree-selection',
 			name: 'downloadDirectory',
-			message: 'Please enter the download Directory?',
+			onlyShowDir: true,
+			message: 'Please select the download Directory?',
+			root: getUserHome(),
 		});
 	}
 
