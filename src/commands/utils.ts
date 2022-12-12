4dba302bf2d456d4eed8233d81fe991654d9fd70
Greg Sithole <gregsithole@hotmail.co.za>
2022-12-12 17:12:24 +0200

    Added additional commands

--- src/commands/utils.ts
+++ src/commands/utils.ts
@@ -1,3 +1,13 @@
+import figlet from 'figlet';
+
 export function getUserHome() {
 	return process.env.HOME || process.env.USERPROFILE;
+}
+
+export function getFilename(url) {
+	return url.substring(url.lastIndexOf('/') + 1);
+}
+
+export function getFigletCLIName() {
+	console.log(figlet.textSync('Wallhaven CLI'));
 }
